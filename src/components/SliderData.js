3b37cc8dd4a2e76db2905af44825cfec64fa5a81
alimmondal <76857187+alimmondal@users.noreply.github.com>
2021-07-16 17:02:06 +0600

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,18 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1626319508670-7ec46cc38421?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1593642634315-48f5414c3ad9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1626285840323-788f8d8764a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1626334418719-e3468f226b8c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1626212680061-3d67e6aa4c22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=821&q=80'
+    },
+
+]
